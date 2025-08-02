9969c29c828f1a36704a007bbb42cec0c0e258a0
Maurice Stemmert <mauricestemmert@MacBook-Pro-von-Maurice.local>
2025-08-02 19:10:22 +0200

    DELETE

--- remoteStorage.js
+++ remoteStorage.js
@@ -1,6 +1,7 @@
 function onloadFunc(){
     console.log("test");
-    postData("/name", {"banana": "rama"});                      //Sendet ein neues Objekt { banana: "rama" } an den Pfad name in deiner Firebase-Datenbank.  Das heißt: Der Eintrag landet unter https://.../name.json.
+    deleteData("/name/-OWfgxieHLX3TE7DACpX");                   //Diese Zeile ruft die Funktion deleteData() auf und löscht einen ganz bestimmten Eintrag unter dem Pfad
+    /name/-OWfgxieHLX3TE7DACpX in deiner Datenbank.
 }
 
 const BASE_URL = "https://remotestorage-a334e-default-rtdb.europe-west1.firebasedatabase.app/";
@@ -19,4 +20,13 @@ async function postData(path="", data={}){                      //Asynchrone Fun
         body: JSON.stringify(data)                              //Der Inhalt der Anfrage – wird mit JSON.stringify() in einen JSON-Text umgewandelt (z. B. {"banana":"rama"}), damit die Datenbank ihn speichern kann.        
     });
     return responseToJson = await response.json();              //Gibt die Antwort der Datenbank zurück (Firebase schickt oft automatisch eine ID mit).
+}
+
+async function deleteData(path=""){                             //Neue asynchrone Funktion, um Daten zu löschen.Der path-Parameter gibt an, was genau gelöscht werden soll
+    
+    let response = await fetch(BASE_URL + path + ".json",{      //Sendet einen DELETE-Request an Firebase – genau wie bei GET und POST, .json muss drangehängt werden.
+        method: "DELETE",                                       //Gibt an, dass es sich um eine Löschanfrage handelt.Dadurch löscht Firebase den angegebenen Eintrag vollständig.  
+        
+    });                           
+    return responseToJson = await response.json();              
 }
